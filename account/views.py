cba2163a7617a6c49417ed707b15e379ed1811d8
dar1in9 <l95475967@163.com>
2020-04-10 19:23:02 +0800

    update

--- account/views.py
+++ account/views.py
@@ -32,12 +32,12 @@ def user_login(request):
 
 
 def detail(request):
-    #if request.method == "POST":
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    checkcode = request.POST.get("checkcode")
-    url = 'http://127.0.0.1:5001/WeBASE-Node-Manager/account/login?checkCode={0}&account={1}&accountPwd={2}'.format(checkcode, username, str(hashlib.sha256(password.encode("utf-8")).hexdigest()))
-    headers = {'Content-type': 'application/json', 'token': token}
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        checkcode = request.POST.get("checkcode")
+        url = 'http://127.0.0.1:5001/WeBASE-Node-Manager/account/login?checkCode={0}&account={1}&accountPwd={2}'.format(checkcode, username, str(hashlib.sha256(password.encode("utf-8")).hexdigest()))
+        headers = {'Content-type': 'application/json', 'token': token}
 
-    r = ses.post(url, headers=headers)
-    return HttpResponse(r.text)
+        r = ses.post(url, headers=headers)
+        return HttpResponse(r.text)
