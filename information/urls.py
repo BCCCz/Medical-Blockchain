da60596c2d90fe3a1fafe779c8ad896f1f3af6fa
dar1in9 <l95475967@163.com>
2020-04-13 23:34:48 +0800

    initpage

--- information/urls.py
+++ information/urls.py
@@ -3,18 +3,8 @@ from . import views
 
 urlpatterns = {
     path('initpage/',views.initpage),
-    path('initdetail/',views.initdetail),
+    path('initpage/initdetail/',views.initdetail),
 
-    path('updatepage/', views.updatepage),
-    path('updatedetail/', views.updatedetail)
 
-    path('querypage/', views.queryepage),
-    path('querydetail/', views.querydetail)
-
-    path('queryepage/', views.querypage),
-    path('recorddetail/', views.querydetail)
-
-    path('medicalquerypage/', views.medicalquerypage),
-    path('medicalquerydetail/', views.medicalquerydetail)
 }
 
