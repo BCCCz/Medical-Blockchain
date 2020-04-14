17c275c7b6f3eb5fc02e670aa8dad92a72b8bb91
YOUNGBChen <455288561@qq.com>
2020-04-14 20:18:42 +0800

    发送交易

--- information/urls.py
+++ information/urls.py
@@ -5,6 +5,18 @@ urlpatterns = {
     path('initpage/',views.initpage),
     path('initpage/initdetail/',views.initdetail),
 
+    path('updatepage/',views.updatepage),
+    path('updatepage/updatedetail/',views.updatedetail),
+
+    path('querypage/',views.querypage),
+    path('querypage/querydetail/',views.querydetail),
+
+    path('recordpage/',views.recordpage),
+    path('recordpage/recorddetail/',views.recordtdetail),
+
+    path('medicalquerypage/',views.medicalquerypage),
+    path('medicalquerypage/medicalquerydetail/',views.medicalquerydetail),
+
 
 }
 
--- information/views.py
+++ information/views.py
@@ -31,9 +31,116 @@ def initdetail(request):
         "contractAbi":[{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"age","type":"int256"},{"name":"tel","type":"uint64"}],"name":"accountUpdate","outputs":[{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"times","type":"uint64"}],"name":"medicalQuery","outputs":[{"name":"","type":"uint64"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint64"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"time","type":"uint64"},{"name":"place","type":"string"},{"name":"info","type":"string"},{"name":"timeStamp","type":"uint64"}],"name":"medicalRecord","outputs":[{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"}],"name":"accountQuery","outputs":[{"name":"","type":"uint64"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint64"},{"name":"","type":"int256"},{"name":"","type":"uint64"},{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"name","type":"string"},{"name":"sex","type":"string"},{"name":"idCard","type":"uint64"},{"name":"age","type":"int256"},{"name":"tel","type":"uint64"}],"name":"accountInit","outputs":[{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"anonymous":"false","inputs":[{"indexed":"false","name":"id","type":"uint64"},{"indexed":"false","name":"name","type":"string"},{"indexed":"false","name":"sex","type":"string"},{"indexed":"false","name":"idCard","type":"uint64"},{"indexed":"false","name":"age","type":"int256"},{"indexed":"false","name":"tel","type":"uint64"}],"name":"accountInitEvent","type":"event"},{"anonymous":"false","inputs":[{"indexed":"false","name":"id","type":"uint64"},{"indexed":"false","name":"age","type":"int256"},{"indexed":"false","name":"tel","type":"uint64"}],"name":"accountUpdateEvent","type":"event"},{"anonymous":"false","inputs":[{"indexed":"false","name":"id","type":"uint64"},{"indexed":"false","name":"time","type":"uint64"},{"indexed":"false","name":"place","type":"string"},{"indexed":"false","name":"info","type":"string"},{"indexed":"false","name":"timeStamp","type":"uint64"}],"name":"medicalRecordEvent","type":"event"}],
         "funcParam":[str(id),str(name),str(sex),str(idCard),str(age),str(tel)],
         "groupId": "1",
-        'user': "0x67b945032e20d6f9041a68e989275ba67870fb9f"
+        'user': "0xd8904921a83ffe1b77b48873eca0f7948a4e8333"
     }
     r = requests.post(url, json=data, headers=headers)
     return HttpResponse(r.text)
 
 
+def updatepage(request):
+    return render(request, "information/accountupdate.html")
+
+def updatedetail(request):
+    id = request.POST.get('id')
+    age = request.POST.get('age')
+    tel = request.POST.get('tel')
+
+    url = 'http://127.0.0.1:5002/WeBASE-Front/trans/handle'
+    headers = {
+        'Content-type': 'application/json',
+    }
+    data = {
+        "useAes": "false",
+        "user": "700001",
+        "contractName": "Medical",
+        "contractAddress": "0xfd0b769192f5a75bbf4f007bcb03a4ad654442e5",
+        "funcName": "accountInit",
+        "contractAbi":[{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"age","type":"int256"},{"name":"tel","type":"uint64"}],"name":"accountUpdate","outputs":[{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"times","type":"uint64"}],"name":"medicalQuery","outputs":[{"name":"","type":"uint64"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint64"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"time","type":"uint64"},{"name":"place","type":"string"},{"name":"info","type":"string"},{"name":"timeStamp","type":"uint64"}],"name":"medicalRecord","outputs":[{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"}],"name":"accountQuery","outputs":[{"name":"","type":"uint64"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint64"},{"name":"","type":"int256"},{"name":"","type":"uint64"},{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"name","type":"string"},{"name":"sex","type":"string"},{"name":"idCard","type":"uint64"},{"name":"age","type":"int256"},{"name":"tel","type":"uint64"}],"name":"accountInit","outputs":[{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"anonymous":"false","inputs":[{"indexed":"false","name":"id","type":"uint64"},{"indexed":"false","name":"name","type":"string"},{"indexed":"false","name":"sex","type":"string"},{"indexed":"false","name":"idCard","type":"uint64"},{"indexed":"false","name":"age","type":"int256"},{"indexed":"false","name":"tel","type":"uint64"}],"name":"accountInitEvent","type":"event"},{"anonymous":"false","inputs":[{"indexed":"false","name":"id","type":"uint64"},{"indexed":"false","name":"age","type":"int256"},{"indexed":"false","name":"tel","type":"uint64"}],"name":"accountUpdateEvent","type":"event"},{"anonymous":"false","inputs":[{"indexed":"false","name":"id","type":"uint64"},{"indexed":"false","name":"time","type":"uint64"},{"indexed":"false","name":"place","type":"string"},{"indexed":"false","name":"info","type":"string"},{"indexed":"false","name":"timeStamp","type":"uint64"}],"name":"medicalRecordEvent","type":"event"}],
+        "funcParam":[str(id),str(age),str(tel)],
+        "groupId": "1",
+        'user': "0xd8904921a83ffe1b77b48873eca0f7948a4e8333"
+    }
+    r = requests.post(url, json=data, headers=headers)
+    return HttpResponse(r.text)
+
+
+def querypage(request):
+    return render(request, "information/accountquery.html")
+
+def querydetail(request):
+    id = request.POST.get('id')
+
+    url = 'http://127.0.0.1:5002/WeBASE-Front/trans/handle'
+    headers = {
+        'Content-type': 'application/json',
+    }
+    data = {
+        "useAes": "false",
+        "user": "700001",
+        "contractName": "Medical",
+        "contractAddress": "0xfd0b769192f5a75bbf4f007bcb03a4ad654442e5",
+        "funcName": "accountInit",
+        "contractAbi":[{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"age","type":"int256"},{"name":"tel","type":"uint64"}],"name":"accountUpdate","outputs":[{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"times","type":"uint64"}],"name":"medicalQuery","outputs":[{"name":"","type":"uint64"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint64"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"time","type":"uint64"},{"name":"place","type":"string"},{"name":"info","type":"string"},{"name":"timeStamp","type":"uint64"}],"name":"medicalRecord","outputs":[{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"}],"name":"accountQuery","outputs":[{"name":"","type":"uint64"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint64"},{"name":"","type":"int256"},{"name":"","type":"uint64"},{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"name","type":"string"},{"name":"sex","type":"string"},{"name":"idCard","type":"uint64"},{"name":"age","type":"int256"},{"name":"tel","type":"uint64"}],"name":"accountInit","outputs":[{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"anonymous":"false","inputs":[{"indexed":"false","name":"id","type":"uint64"},{"indexed":"false","name":"name","type":"string"},{"indexed":"false","name":"sex","type":"string"},{"indexed":"false","name":"idCard","type":"uint64"},{"indexed":"false","name":"age","type":"int256"},{"indexed":"false","name":"tel","type":"uint64"}],"name":"accountInitEvent","type":"event"},{"anonymous":"false","inputs":[{"indexed":"false","name":"id","type":"uint64"},{"indexed":"false","name":"age","type":"int256"},{"indexed":"false","name":"tel","type":"uint64"}],"name":"accountUpdateEvent","type":"event"},{"anonymous":"false","inputs":[{"indexed":"false","name":"id","type":"uint64"},{"indexed":"false","name":"time","type":"uint64"},{"indexed":"false","name":"place","type":"string"},{"indexed":"false","name":"info","type":"string"},{"indexed":"false","name":"timeStamp","type":"uint64"}],"name":"medicalRecordEvent","type":"event"}],
+        "funcParam":[str(id)],
+        "groupId": "1",
+        'user': "0xd8904921a83ffe1b77b48873eca0f7948a4e8333"
+    }
+    r = requests.post(url, json=data, headers=headers)
+    return HttpResponse(r.text)
+
+
+def recordpage(request):
+    return render(request, "information/medicalrecord.html")
+
+def recordtdetail(request):
+    id = request.POST.get('id')
+    time = request.POST.get('time')
+    place = request.POST.get('place')
+    info = request.POST.get('info')
+    timeStamp = request.POST.get('timeStamp')
+
+
+    url = 'http://127.0.0.1:5002/WeBASE-Front/trans/handle'
+    headers = {
+        'Content-type': 'application/json',
+    }
+    data = {
+        "useAes": "false",
+        "user": "700001",
+        "contractName": "Medical",
+        "contractAddress": "0xfd0b769192f5a75bbf4f007bcb03a4ad654442e5",
+        "funcName": "accountInit",
+        "contractAbi":[{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"age","type":"int256"},{"name":"tel","type":"uint64"}],"name":"accountUpdate","outputs":[{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"times","type":"uint64"}],"name":"medicalQuery","outputs":[{"name":"","type":"uint64"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint64"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"time","type":"uint64"},{"name":"place","type":"string"},{"name":"info","type":"string"},{"name":"timeStamp","type":"uint64"}],"name":"medicalRecord","outputs":[{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"}],"name":"accountQuery","outputs":[{"name":"","type":"uint64"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint64"},{"name":"","type":"int256"},{"name":"","type":"uint64"},{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"name","type":"string"},{"name":"sex","type":"string"},{"name":"idCard","type":"uint64"},{"name":"age","type":"int256"},{"name":"tel","type":"uint64"}],"name":"accountInit","outputs":[{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"anonymous":"false","inputs":[{"indexed":"false","name":"id","type":"uint64"},{"indexed":"false","name":"name","type":"string"},{"indexed":"false","name":"sex","type":"string"},{"indexed":"false","name":"idCard","type":"uint64"},{"indexed":"false","name":"age","type":"int256"},{"indexed":"false","name":"tel","type":"uint64"}],"name":"accountInitEvent","type":"event"},{"anonymous":"false","inputs":[{"indexed":"false","name":"id","type":"uint64"},{"indexed":"false","name":"age","type":"int256"},{"indexed":"false","name":"tel","type":"uint64"}],"name":"accountUpdateEvent","type":"event"},{"anonymous":"false","inputs":[{"indexed":"false","name":"id","type":"uint64"},{"indexed":"false","name":"time","type":"uint64"},{"indexed":"false","name":"place","type":"string"},{"indexed":"false","name":"info","type":"string"},{"indexed":"false","name":"timeStamp","type":"uint64"}],"name":"medicalRecordEvent","type":"event"}],
+        "funcParam":[str(id),str(time),str(place),str(info),str(timeStamp)],
+        "groupId": "1",
+        'user': "0xd8904921a83ffe1b77b48873eca0f7948a4e8333"
+    }
+    r = requests.post(url, json=data, headers=headers)
+    return HttpResponse(r.text)
+
+
+def medicalquerypage(request):
+    return render(request, "information/meidcalquery.html")
+
+def medicalquerydetail(request):
+    id = request.POST.get('id')
+    time = request.POST.get('time')
+
+
+    url = 'http://127.0.0.1:5002/WeBASE-Front/trans/handle'
+    headers = {
+        'Content-type': 'application/json',
+    }
+    data = {
+        "useAes": "false",
+        "user": "700001",
+        "contractName": "Medical",
+        "contractAddress": "0xfd0b769192f5a75bbf4f007bcb03a4ad654442e5",
+        "funcName": "accountInit",
+        "contractAbi":[{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"age","type":"int256"},{"name":"tel","type":"uint64"}],"name":"accountUpdate","outputs":[{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"times","type":"uint64"}],"name":"medicalQuery","outputs":[{"name":"","type":"uint64"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint64"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"time","type":"uint64"},{"name":"place","type":"string"},{"name":"info","type":"string"},{"name":"timeStamp","type":"uint64"}],"name":"medicalRecord","outputs":[{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"}],"name":"accountQuery","outputs":[{"name":"","type":"uint64"},{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint64"},{"name":"","type":"int256"},{"name":"","type":"uint64"},{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"constant":"false","inputs":[{"name":"id","type":"uint64"},{"name":"name","type":"string"},{"name":"sex","type":"string"},{"name":"idCard","type":"uint64"},{"name":"age","type":"int256"},{"name":"tel","type":"uint64"}],"name":"accountInit","outputs":[{"name":"","type":"int256"}],"payable":"false","stateMutability":"nonpayable","type":"function"},{"anonymous":"false","inputs":[{"indexed":"false","name":"id","type":"uint64"},{"indexed":"false","name":"name","type":"string"},{"indexed":"false","name":"sex","type":"string"},{"indexed":"false","name":"idCard","type":"uint64"},{"indexed":"false","name":"age","type":"int256"},{"indexed":"false","name":"tel","type":"uint64"}],"name":"accountInitEvent","type":"event"},{"anonymous":"false","inputs":[{"indexed":"false","name":"id","type":"uint64"},{"indexed":"false","name":"age","type":"int256"},{"indexed":"false","name":"tel","type":"uint64"}],"name":"accountUpdateEvent","type":"event"},{"anonymous":"false","inputs":[{"indexed":"false","name":"id","type":"uint64"},{"indexed":"false","name":"time","type":"uint64"},{"indexed":"false","name":"place","type":"string"},{"indexed":"false","name":"info","type":"string"},{"indexed":"false","name":"timeStamp","type":"uint64"}],"name":"medicalRecordEvent","type":"event"}],
+        "funcParam":[str(id),str(time)],
+        "groupId": "1",
+        'user': "0xd8904921a83ffe1b77b48873eca0f7948a4e8333"
+    }
+    r = requests.post(url, json=data, headers=headers)
+    return HttpResponse(r.text)
